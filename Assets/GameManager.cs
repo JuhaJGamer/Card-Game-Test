d7f5c5b0578464444d229b03dcb5d3f07bab3183
JuhaJGamer <juhajgamer@live.fi>
2017-01-10 08:25:17 +0200

    Added sprites, did some code
    Startin dev on the project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public NetworkPlayer[] players;
+    public GameObject[] places;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        //If client, destroy
+        if (Network.isClient)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get all players
+        players = Network.connections;
+        //
+        
+    }
+}
---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public NetworkPlayer[] players;
+    public GameObject[] places;
+    public GameObject cardStock;
+    public Sprite[] cards;
+    public GameObject card;
+
+	// Use this for initialization
+	void Start () {
+        //Not needed but just in case
+		if(!Network.isClient)
+        {
+            Destroy(gameObject);
+        }
+
+        //Get players
+        players = Network.connections;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //Get players
+        players = Network.connections;
+        //Set player positions
+
+	}
+}
